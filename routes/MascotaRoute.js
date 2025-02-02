d82f9aaee12f2b0ea06e93378ee1394d227fdc03
Esteban <estebanmar856@gmail.com>
2025-02-02 20:24:52 -0300

    pruebas

--- routes/MascotaRoute.js
+++ routes/MascotaRoute.js
@@ -2,7 +2,6 @@ import express from 'express'
 import ModelMascota from '../model/Mascota.js'
 import passport from 'passport';
 import mongoose from 'mongoose';
-import ModelCliente from '../model/Cliente.js';
 
 
 const MascostaRouter = express()
@@ -96,13 +95,8 @@ MascostaRouter.get('/mascota/busqueda', passport.authenticate("jwt", { session: 
             return res.status(400).json({ mensaje: 'El parámetro cliente es requerido' });
         }
 
-        const clienteEncontrado = await ModelCliente.findOne({ nombre: cliente });
-
-        if (!clienteEncontrado) {
-            return res.status(404).json({ mensaje: 'Cliente no encontrado' });
-        }
        
-        const mascotas = await ModelMascota.find({ cliente: clienteEncontrado._id, usuario: req.user.id }).populate('cliente');
+        const mascotas = await ModelMascota.find({ cliente, usuario: req.user.id }).populate('cliente');
 
         if (!mascotas.length) {
             return res.status(404).json({ mensaje: 'No se encontraron mascotas asociadas al cliente especificado' });
