7eb43db8ba661cce31bed07c9310de99cbed8e41
Esteban <estebanmar856@gmail.com>
2025-02-02 20:48:43 -0300

    pruebas

--- routes/MascotaRoute.js
+++ routes/MascotaRoute.js
@@ -2,7 +2,6 @@ import express from 'express'
 import ModelMascota from '../model/Mascota.js'
 import passport from 'passport';
 import mongoose from 'mongoose';
-import ModelCliente from '../model/Cliente.js';
 
 
 const MascostaRouter = express()
@@ -88,27 +87,16 @@ MascostaRouter.delete("/mascota/:id", passport.authenticate("jwt", { session: fa
         res.status(400).send({ mensaje: "Error al eliminar", error });
     }
 })
-import mongoose from 'mongoose';
-import ModelMascota from './models/mascota';  // Asegúrate de importar correctamente tu modelo
-import ModelCliente from './models/cliente';  // Asegúrate de importar el modelo de cliente
 
-MascostaRouter.get('/mascota/busqueda', passport.authenticate("jwt", { session: false }), async (req, res) => {
+MascostaRouter.get('/gestion/mascota/busqueda', passport.authenticate("jwt", { session: false }), async (req, res) => {
     const { cliente } = req.query;
-    
     try {
         if (!cliente) {
             return res.status(400).json({ mensaje: 'El parámetro cliente es requerido' });
         }
-        let clienteId = cliente;
-        if (!mongoose.Types.ObjectId.isValid(cliente)) {
-            
-            const clienteEncontrado = await ModelCliente.findOne({ nombre: cliente });
-            if (!clienteEncontrado) {
-                return res.status(404).json({ mensaje: 'Cliente no encontrado' });
-            }
-            clienteId = clienteEncontrado._id;
-        }
-        const mascotas = await ModelMascota.find({ cliente: clienteId }).populate('cliente');
+
+       
+        const mascotas = await ModelMascota.find({ cliente, usuario: req.user.id }).populate('cliente');
 
         if (!mascotas.length) {
             return res.status(404).json({ mensaje: 'No se encontraron mascotas asociadas al cliente especificado' });
@@ -119,7 +107,6 @@ MascostaRouter.get('/mascota/busqueda', passport.authenticate("jwt", { session: 
         res.status(500).json({ mensaje: 'No se pudo obtener las mascotas', error: error.message });
     }
 });
-
 
 
 
